ccc591be27c0936f783ec64ec83cbe6d70b1d918
Razmik <razmik.unanyan@mail.ru>
2025-01-24 12:32:24 -0600

    fixed build

--- src/components/Languages/languages.props.ts
+++
@@ -1,3 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
-
-export interface ILanguagesProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,4 @@ export * from "../routes/AnimatedRoutes/AnimatedRoutes";
 export * from "./PageTitle/PageTitle";
 export * from "./Shapes/Shapes";
 export * from "./Socials/Socials";
-export * from "./Languages/Languages";
 export * from "./Notification/Notification";
