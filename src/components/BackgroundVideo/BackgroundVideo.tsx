69e03c637f39fef5872cd9d31b177fd64a07b44a
Razmik <razmik.unanyan@mail.ru>
2025-01-24 18:22:24 -0600

    added webm

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -7,6 +7,7 @@ const BackgroundVideo: React.FC = () => {
       <div className={styles.mask}/>
       <video autoPlay loop muted className={styles.backgroundVideo}>
         <source src="/video/video_1.mp4" type="video/mp4" />
+        <source src="/video/video_1.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
     </div>
