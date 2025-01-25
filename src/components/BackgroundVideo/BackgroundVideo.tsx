fd8295dbcbbc67948ec971d13e3ce6c1b6b87478
Razmik <razmik.unanyan@mail.ru>
2025-01-24 18:14:18 -0600

    fixed url

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./backgroundVideo.module.scss";
-// @ts-ignore
-import video from './video.mp4'
 
 const BackgroundVideo: React.FC = () => {
   return (
     <div className={styles.backgroundVideoContainer}>
       <div className={styles.mask}/>
       <video autoPlay loop muted className={styles.backgroundVideo}>
-        <source src={video} type="video/mp4" />
+        <source src="/video/video_1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
