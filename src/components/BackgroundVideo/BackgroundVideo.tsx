cf25acb9efa38709b771cbeff49ed20039c3f5be
Razmik <razmik.unanyan@mail.ru>
2025-01-25 19:12:20 -0600

    update BackgroundVideo

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -6,7 +6,7 @@ const BackgroundVideo: React.FC = () => {
     <div className={styles.backgroundVideoContainer}>
       <div className={styles.mask}/>
       <video autoPlay loop muted className={styles.backgroundVideo}>
-        <source src="https://runnerview.s3.us-east-2.amazonaws.com/back.mp4" type="video/mp4" />
+        <source src="https://d28hdws53j511j.cloudfront.net/back.mp4" type="video/mp4" />
       </video>
     </div>
   );
