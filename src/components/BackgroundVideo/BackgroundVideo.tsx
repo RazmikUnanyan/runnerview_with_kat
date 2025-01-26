4515cc8d2ae8bd9fe085f76673384c912aa6a7be
Razmik <razmik.unanyan@mail.ru>
2025-01-26 14:00:59 -0600

    Video Optimization

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./backgroundVideo.module.scss";
 
 const BackgroundVideo: React.FC = () => {
+
   return (
     <div className={styles.backgroundVideoContainer}>
-      <div className={styles.mask}/>
-      <video autoPlay loop muted className={styles.backgroundVideo}>
-        <source src="https://d28hdws53j511j.cloudfront.net/back.mp4" type="video/mp4" />
+      <div className={styles.mask} />
+      <video poster="/img_1.png" playsInline autoPlay loop muted className={styles.backgroundVideo}>
+        <source src="https://d28hdws53j511j.cloudfront.net/videobackrun.webm" type="video/webm" />
+        <source src="https://d28hdws53j511j.cloudfront.net/videobackrun.mp4" type="video/mp4" />
       </video>
     </div>
   );
