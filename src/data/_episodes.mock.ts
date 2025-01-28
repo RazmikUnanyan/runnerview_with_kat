a805f543f32170e1da9257afd7cca72188f35704
Razmik <razmik.unanyan@mail.ru>
2025-01-27 21:01:08 -0600

    update mock

--- src/data/_episodes.mock.ts
+++ src/data/_episodes.mock.ts
@@ -8,5 +8,7 @@ export const IEpisode_MOCK: IEpisode[] = [
     image: img1,
     id: 1,
     number: "Lisa Thompson. #1",
+    title: "Visually Impaired Runner Lisa Thompson: I am NOT quitting",
+    description: "Runnerview with Kat & Lisa Thompson @modernbnb the owner of Modern City Properties @moderncityproperties, a visionary entrepreneur in the real estate industry. Lisa is also a runner! She won visually impaired division of the Boston Marathon, with Olympic runner and award-winning filmmaker Alexi Pappas @alexipappas as her guide. Twice the President of BCRR (Bayou City Road Runners) @houstonBCRR a local prominent running club. @runnerviewwithkat"
   }
 ];
