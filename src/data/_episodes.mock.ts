6dc51328310a61708e48ba3f0c2eadf2269d819b
Razmik <razmik.unanyan@mail.ru>
2025-02-01 10:11:54 -0600

    update mocks

--- src/data/_episodes.mock.ts
+++ src/data/_episodes.mock.ts
@@ -1,5 +1,6 @@
 import { IEpisode } from "../interface";
 import img1 from "../assets/episodes/img_1.png"
+import img2 from "../assets/episodes/img2.png"
 
 export const IEpisode_MOCK: IEpisode[] = [
   {
@@ -10,5 +11,14 @@ export const IEpisode_MOCK: IEpisode[] = [
     number: "Lisa Thompson. #1",
     title: "Visually Impaired Runner Lisa Thompson: I am NOT quitting",
     description: "Runnerview with Kat & Lisa Thompson @modernbnb the owner of Modern City Properties @moderncityproperties, a visionary entrepreneur in the real estate industry. Lisa is also a runner! She won visually impaired division of the Boston Marathon, with Olympic runner and award-winning filmmaker Alexi Pappas @alexipappas as her guide. Twice the President of BCRR (Bayou City Road Runners) @houstonBCRR a local prominent running club. @runnerviewwithkat"
+  },
+  {
+    linkYouTube: "https://www.youtube.com/watch?v=cSx46OZGbzc&t=232s",
+    linkSpotify: "https://open.spotify.com/show/6TRR1IVOe4VpjaDZH2M9LD?si=XK3cNdXMQna3POOZEg_opw",
+    image: img2,
+    id: 2,
+    number: "Bob Schamus. #2",
+    title: "Bob Schamus: Getting Stronger, One Race At A Time",
+    description: "Meet Bob Schamus - 20+ marathon finisher, multiple iron man finisher, former President of Houston striders - the largest running club in Houston - and someone who always remembers your name. We discuss why we come back to running marathons despite the pain, the benefits of running clubs, lasting friendships and so much more."
   }
 ];
