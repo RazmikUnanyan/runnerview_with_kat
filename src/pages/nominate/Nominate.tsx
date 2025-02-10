12602b22ca3f9fab30e5a2c26757977938c86aca
Razmik <razmik.unanyan@mail.ru>
2025-02-10 10:38:47 -0600

    added stub

--- src/pages/nominate/Nominate.tsx
+++ src/pages/nominate/Nominate.tsx
@@ -16,10 +16,11 @@ const initialForm = {
   theirEmail: "",
   link: "",
   message: "",
-}
+};
 export const Nominate: FC<INominateProps> = () => {
   const [formData, setFormData] = useState(initialForm);
   const [filledFields, setFilledFields] = useState(0);
+  const [isSuccessfully, setIsSuccessfully] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
@@ -32,22 +33,21 @@ export const Nominate: FC<INominateProps> = () => {
   const sendEmail = (e: React.FormEvent) => {
     e.preventDefault();
 
-
     emailjs
       .send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID || '',
-        process.env.REACT_APP_EMAILJS_NOMINATE_TEMPLATE_ID || '',
+        process.env.REACT_APP_EMAILJS_SERVICE_ID || "",
+        process.env.REACT_APP_EMAILJS_NOMINATE_TEMPLATE_ID || "",
         formData,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY || '',
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY || ""
       )
       .then(
         (result) => {
-          console.log("Email sent successfully:", result.text);
-          setFormData(initialForm)
+          setIsSuccessfully(true);
+          setFormData(initialForm);
         },
         (error) => {
           console.error("Email sending failed:", error.text);
-          setFormData(initialForm)
+          setFormData(initialForm);
         }
       );
   };
@@ -60,107 +60,117 @@ export const Nominate: FC<INominateProps> = () => {
   return (
     <motion.section className={styles.contacts_wrapper} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <PageTitle>Nominate</PageTitle>
-      <div className={styles.contacts}>
-        <div className={styles.contacts_info}>
-          We all have someone who has <span>inspired</span> us, believed in us, and made a lasting impact on our
-          journey. Now it's your chance to share <span>their story</span> and inspire others! Nominate a podcast guest
-          and help us
-          <span> spotlight</span> the voices that deserve to be heard.
+      {isSuccessfully ? (
+        <div className={styles.stub}>
+          <h2>We are super pumped!</h2>
+          <p> Your nomination has been submitted.</p>
         </div>
-        <form onSubmit={sendEmail} className={styles.contacts_form}>
-          Your Name
-          <div className={styles.contacts_form_group}>
+      ) : (
+        <div className={styles.contacts}>
+          <div className={styles.contacts_info}>
+            We all have someone who has <span>inspired</span> us, believed in us, and made a lasting impact on our
+            journey. Now it's your chance to share <span>their story</span> and inspire others! Nominate a podcast guest
+            and help us
+            <span> spotlight</span> the voices that deserve to be heard.
+          </div>
+          <form onSubmit={sendEmail} className={styles.contacts_form}>
+            Your Name
+            <div className={styles.contacts_form_group}>
+              <div className={styles.contacts_form_input_wrapper}>
+                <Input
+                  type="text"
+                  name="yourFirstName"
+                  placeholder="first name"
+                  value={formData.yourFirstName}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className={styles.contacts_form_input_wrapper}>
+                <Input
+                  type="text"
+                  name="yourLastName"
+                  placeholder="last name"
+                  value={formData.yourLastName}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+            Your Email
             <div className={styles.contacts_form_input_wrapper}>
               <Input
-                type="text"
-                name="yourFirstName"
-                placeholder="first name"
-                value={formData.yourFirstName}
+                type="email"
+                name="email"
+                placeholder="email"
+                value={formData.email}
                 onChange={handleChange}
                 required
               />
             </div>
+            Whom would you like to nominate?
+            <div className={styles.contacts_form_group}>
+              <div className={styles.contacts_form_input_wrapper}>
+                <Input
+                  type="text"
+                  name="firstName"
+                  placeholder="first name"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className={styles.contacts_form_input_wrapper}>
+                <Input
+                  type="text"
+                  name="lastName"
+                  placeholder="last name"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+            Their Email
             <div className={styles.contacts_form_input_wrapper}>
               <Input
                 type="text"
-                name="yourLastName"
-                placeholder="last name"
-                value={formData.yourLastName}
+                name="theirEmail"
+                placeholder="email"
+                value={formData.theirEmail}
+                onChange={handleChange}
+              />
+            </div>
+            What's their IG account?
+            <div className={styles.contacts_form_input_wrapper}>
+              <Input
+                type="text"
+                name="link"
+                placeholder="account"
+                value={formData.link}
                 onChange={handleChange}
                 required
               />
             </div>
-          </div>
-          Your Email
-          <div className={styles.contacts_form_input_wrapper}>
-            <Input
-              type="email"
-              name="email"
-              placeholder="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          Whom would you like to nominate?
-          <div className={styles.contacts_form_group}>
-            <div className={styles.contacts_form_input_wrapper}>
-              <Input
-                type="text"
-                name="firstName"
-                placeholder="first name"
-                value={formData.firstName}
+            Why do you think they would be an inspiring guest?
+            <div className={cn(styles.contacts_form_input_wrapper, styles.contacts_form_input_area)}>
+              <Textarea
+                name="message"
+                placeholder="text"
+                rows={10}
+                value={formData.message}
                 onChange={handleChange}
-                required
-              />
+                required></Textarea>
             </div>
-            <div className={styles.contacts_form_input_wrapper}>
-              <Input
-                type="text"
-                name="lastName"
-                placeholder="last name"
-                value={formData.lastName}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </div>
-          Their Email
-          <div className={styles.contacts_form_input_wrapper}>
-            <Input
-              type="text"
-              name="theirEmail"
-              placeholder="email"
-              value={formData.theirEmail}
-              onChange={handleChange}
-            />
-          </div>
-          What's their IG account?
-          <div className={styles.contacts_form_input_wrapper}>
-            <Input
-              type="text"
-              name="link"
-              placeholder="account"
-              value={formData.link}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          Why do you think they would be an inspiring guest?
-          <div className={cn(styles.contacts_form_input_wrapper, styles.contacts_form_input_area)}>
-            <Textarea
-              name="message"
-              placeholder="text"
-              rows={10}
-              value={formData.message}
-              onChange={handleChange}
-              required></Textarea>
-          </div>
-          <Button percentageFilled={15 * filledFields} type="submit" variant={filledFields < 7 ? "outline" : "primary"}>
-            Send
-          </Button>
-        </form>
-      </div>
+            <Button
+              percentageFilled={15 * filledFields}
+              type="submit"
+              variant={filledFields < 7 ? "outline" : "primary"}>
+              Send
+            </Button>
+          </form>
+        </div>
+      )}
     </motion.section>
   );
 };
