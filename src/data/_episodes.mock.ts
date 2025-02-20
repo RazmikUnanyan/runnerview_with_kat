8a8cfdcdd2349d799acc46e49675d409b9b470ab
Razmik <razmik.unanyan@mail.ru>
2025-02-20 16:03:41 -0600

    added episodes

--- src/data/_episodes.mock.ts
+++ src/data/_episodes.mock.ts
@@ -1,6 +1,7 @@
 import { IEpisode } from "../interface";
 import img1 from "../assets/episodes/img_1.png"
 import img2 from "../assets/episodes/img2.png"
+import img3 from "../assets/episodes/img3.jpg"
 
 export const IEpisode_MOCK: IEpisode[] = [
   {
@@ -20,5 +21,14 @@ export const IEpisode_MOCK: IEpisode[] = [
     number: "Bob Schamus",
     title: "Bob Schamus: Getting Stronger, One Race At A Time",
     description: "Meet Bob Schamus - 20+ marathon finisher, multiple iron man finisher, former President of Houston striders - the largest running club in Houston - and someone who always remembers your name. We discuss why we come back to running marathons despite the pain, the benefits of running clubs, lasting friendships and so much more."
+  },
+  {
+    linkYouTube: "https://www.youtube.com/watch?v=HEgdENr8PFw",
+    linkSpotify: "https://open.spotify.com/show/6TRR1IVOe4VpjaDZH2M9LD?si=XK3cNdXMQna3POOZEg_opw",
+    image: img3,
+    id: 3,
+    number: "Maggie Lentz",
+    title: "Maggie Lentz",
+    description: ""
   }
 ];
